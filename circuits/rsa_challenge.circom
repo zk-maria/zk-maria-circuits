176542abf53ddfcf2f89a632ace4105076288c9d
ArturVargas <arturvargas9226@gmail.com>
2024-05-18 23:39:02 -0600

    add circuits

--- circuits/rsa_challenge.circom
+++ circuits/rsa_challenge.circom
@@ -1,16 +1,79 @@
-pragma circom 2.0.0;
+pragma circom 2.1.6;
 
-/*This circuit template checks that c is the multiplication of a and b.*/  
+include "node_modules/circomlib/circuits/comparators.circom";
 
-template RsaChallenge () {  
+template MatchingNationality(nation) {  
 
    // Declaration of signals.  
-   signal input a;  
-   signal input b;  
-   signal output c;  
+   signal input asciiArray[nation];
+   signal output result;
 
+   var sum = 0;
    // Constraints.  
-   c <== a * b;  
+   for (var i = 0; i < nation; i++) {
+      //asign
+       sum = sum + asciiArray[i];
+    }
+
+   result <== sum;
 }
 
-component main = RsaChallenge();
+template CheckAge() {
+   // Declaration of signals. 
+   signal input age;
+   signal output isGreaterOrEqual;
+
+   component gt = GreaterThan(5);
+    gt.in[0] <== age;
+    gt.in[1] <== 18;
+	0 === gt.out;
+    
+   isGreaterOrEqual <== gt.out;
+}
+
+template ExpiryDateID() {
+   // Declaration of signals. 
+   signal input expiryDate;
+   signal input currentDate;
+
+   signal output isExpired;
+
+   // Temp variable to check if expiryDate - current date + 30 days is non-negative
+    signal temp;
+
+    temp <== expiryDate - (currentDate + 30);
+
+    component gt = GreaterThan(7);
+    gt.in[0] <== temp;
+    gt.in[1] <== 0;
+	 0 === gt.out;
+    // isExpired is 1 if temp is non-negative, 0 otherwise
+    isExpired <== gt.out;
+}
+
+template MainCircuit(nationality) {
+   signal input b;
+   signal input asciiArray[nationality];
+   signal input cd;
+   signal input ed;
+
+   signal output isMexican;
+   signal output isGreaterOrEqual;
+   signal output isExpired;
+
+   component nation = MatchingNationality(nationality);
+   component age = CheckAge();
+   component expiryDate = ExpiryDateID();
+
+   nation.asciiArray <== asciiArray;
+   age.age <== b;
+   expiryDate.expiryDate <== ed;
+   expiryDate.currentDate <== cd;
+
+   // outputs
+   isMexican <== nation.result;
+   isGreaterOrEqual <== age.isGreaterOrEqual;
+   isExpired <== expiryDate.isExpired;
+}
+
+component main = MainCircuit(7);
--- contracts/contracts/RsaChallenge.sol
+++ contracts/contracts/RsaChallenge.sol
@@ -9,40 +9,41 @@ contract RsaChallenge {
     address payable public owner;
     Groth16Verifier private verifier;
 
-    mapping(uint256 => uint256) public challenges;
-    mapping(uint256 => uint256) private challengesSet;
-    mapping(uint256 => address) public challengesCompleted;
+    mapping(uint256 => uint256) public requests;
+    mapping(uint256 => uint256) private requestsSet;
+    mapping(uint256 => address) public requestsCompleted;
 
-    uint256 public challengesCount;
+    uint256 public requestsCount;
 
-    event Verified(address user, uint256 challenge);
+    event Verified(address user, uint256 requestId);
 
     constructor(address snarkVerifier) payable {
         verifier = Groth16Verifier(snarkVerifier);
         owner = payable(msg.sender);
-        challengesCount = 0;
+        requestsCount = 0;
     }
 
-    function newChallenge(uint256 challenge) public {
-        require(msg.sender == owner, "You aren't the owner");
-        require(challengesSet[challenge] == 0, "Challenge already exists");
-        challengesSet[challenge] = 1;
-        challenges[challengesCount] = challenge;
-        challengesCount++;
+    function newRequest(uint256 requestId) public {
+        // require(msg.sender == owner, "You aren't the owner");
+        require(requestsSet[requestId] == 0, "Request already exists");
+        requestsSet[requestId] = 1;
+        requests[requestsCount] = requestId;
+        requestsCount++;
     }
 
-    function solveChallenge(uint256 challenge, uint256[8] memory proof) public {
-        require(challengesSet[challenge] == 1, "Challenge does not exist");
-        require(challengesCompleted[challenge] == address(0), "Challenge already solved");
+    function verifyRequest(uint256 requestId, uint256[8] memory proof) public {
+        require(requestsSet[requestId] == 1, "Request does not exist");
+        require(requestsCompleted[requestId] == address(0), "Request already acepted");
 
-        uint256[2] memory pA = [proof[0], proof[1]];
-        uint256[2][2] memory pB = [[proof[2], proof[3]], [proof[4], proof[5]]];
+        uint256[2] memory pNA = [proof[0], proof[1]];
+        uint256[2][2] memory pAge = [[proof[2], proof[3]], [proof[4], proof[5]]];
+        // pED expiry date
         uint256[2] memory pC = [proof[6], proof[7]];
 
-        require(verifier.verifyProof(pA, pB, pC, [challenge]), "Invalid proof");
+        require(verifier.verifyProof(pA, pB, pC, [requestId]), "Invalid proof");
 
-        challengesCompleted[challenge] = msg.sender;
-        emit Verified(msg.sender, challenge);
+        requestsCompleted[requestId] = msg.sender;
+        emit Verified(msg.sender, requestId);
     }
 
 }
